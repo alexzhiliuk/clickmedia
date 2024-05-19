7cc34397c2bc470242d599862fcd603eaa59f172
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2024-05-19 15:11:55 +0300

    result section

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,8 @@ import * as tabs from './modules/tabs.js';
 import * as modal from './modules/modal.js';
 import * as caseSlider from './modules/caseSlider.js';
 
+import Chart from 'chart.js/auto';
+
 
 
 flsFunctions.isWebp()
@@ -31,3 +33,73 @@ if (Object.keys(sectionsOffset).length > 0) {
         }
     })
 }
+
+const ctx = document.getElementById('myChart');
+const DATA_COUNT = 7;
+
+
+const data = {
+  labels: ["Янв.", "Фев.", "Март", "Апр.", "Май", "Июнь", "Июль", "Июль", "Авг.", "Сент.", "Окт.", "Нояб.", "Дек."],
+  datasets: [
+    {
+      data: {
+        "Янв.": 6,
+        "Фев.": 10,
+        "Март": 6,
+        "Апр.":  30,
+        "Май": 9, 
+        "Июнь": 10,
+        "Июль": 25, 
+        "Июль":  6,
+        "Авг.":  9,
+        "Сент.":  25,
+        "Окт.": 60,
+        "Нояб.": 70, 
+        "Дек.": 75
+      },
+      borderColor: "#FF0000",
+      backgroundColor: "rgba(255, 0, 0, 0.5)",
+    },
+
+  ]
+};
+
+// class MyScale extends Chart.Scale {
+//     /* extensions ... */
+// }
+// MyScale.id = 'myScale';
+// MyScale.defaults = defaultConfigObject;
+
+// new Chart(ctx, {
+//     type: 'line',
+//     data: data,
+//     options: {
+//         responsive: true,
+//         plugins: {
+//             legend: {
+//                 position: 'top',
+//             },
+//             title: {
+//                 display: true,
+//                 text: 'Chart.js Line Chart'
+//             }
+//         },
+//         scales: {
+//             y: {
+//                 // beginAtZero: true,
+//                 ticks: {
+//                     callback: function(value) {
+//                         const customLabels = [0, 5, 10, 75, 100];
+//                         if (customLabels.includes(value)) {
+//                             return value;
+//                         }
+//                         return null;
+//                     },
+//                     stepSize: 1
+//                 }
+            
+                
+//             }
+//         },
+//     },
+// });
