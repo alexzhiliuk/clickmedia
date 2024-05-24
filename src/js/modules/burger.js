ac3d2f8c06687486332e976eeaed322c4a15556f
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2024-05-24 18:21:05 +0300

    fix scroll header

--- src/js/modules/burger.js
+++ src/js/modules/burger.js
@@ -56,29 +56,22 @@ $(".header__burger").click(function() {
 
 })
 
+
 var lastScrollTop = 0;
+var lastWindowWidth = window.outerWidth
 $(window).scroll(function(event){
+
+    if (lastWindowWidth != window.outerWidth) {
+        lastScrollTop = $(this).scrollTop();
+        lastWindowWidth = window.outerWidth
+        return
+    }
 
     var st = $(this).scrollTop();
     if (st > lastScrollTop){
         $(".header").addClass("header_scrolled")
     } else {
         $(".header").removeClass("header_scrolled")
-
-        if ($(".header__burger").hasClass('header__burger_active')) {
-            $(".header__burger").removeClass("header__burger_active")
-            let $menu = $(".header__menu"),
-                contact = $(".header__contact")
-    
-            $menu.removeClass("header__menu_active")
-            $menu.insertAfter(".header__logo")
-            contact.removeClass("header__contact_active")
-            $('.drop-menu').hide();
-            $(".header__menu").css("overflow", "")
-            $(".header__menu-link").removeClass("active")
-            $("body, html").removeClass("lock")
-        }
-
     }
     lastScrollTop = st;
 
